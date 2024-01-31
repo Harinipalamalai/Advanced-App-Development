3215e7f38749b8029e1fb6025d06856914ad6bfc
Harinipalamalai <114054884+Harinipalamalai@users.noreply.github.com>
2024-01-31 09:30:55 +0530

    Add files via upload

---
+++ Front end/day 5/Submit.jsx
@@ -0,0 +1,42 @@
+
+import './Submit.css';
+function Submit() {
+  return (
+    <div className="backq11">
+      <div className="table77">
+      <div className="grid-container">
+  <div className="grid-item">Applicant Name: Harini P</div>
+  <div className="grid-item">Applicant phone: 9988776655</div>
+  <div className="grid-item">Applicant Loan Id: 556677</div> 
+</div>
+<div className="grid-container2">
+  <div className="grid-item2">Applicant Email: harinipalanaXXX@gmail.com</div>
+  </div>
+  <div className="grid-container3">
+  <div className="grid-item3">Applicant Pan no: 2398xxxxx</div>
+  <div class="grid-item3"></div>
+  <div class="grid-item3"><button className='accreg'>Accepted</button></div>
+  </div>
+      </div>
+      <div className="table77">
+      <div className="grid-container">
+  <div className="grid-item">Applicant Name: Ruba P</div>
+  <div className="grid-item">Applicant phone: 9955435272</div>
+  <div className="grid-item">Applicant Loan Id: 356677</div> 
+</div>
+<div className="grid-container2">
+  <div className="grid-item2">Applicant Email: rubapalanaXXX@gmail.com</div>
+  </div>
+  <div className="grid-container3">
+  <div className="grid-item3">Applicant Pan no: 7598xxxxx</div>
+  <div class="grid-item3"></div>
+  <div class="grid-item3"><button className='accreg2'>Rejected</button></div>
+  </div>
+      </div>
+    </div>
+    
+   
+  )
+}
+
+export default Submit;
